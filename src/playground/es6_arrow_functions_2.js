79f842b7bdb69cf6d7e5f11da46ebcc232bba8b8
SnowSurfer <drinkaziu@gmail.com>
2018-12-25 21:20:53 -0500

    arrow functions 2

--- src/playground/es6_arrow_functions_2.js
+++ src/playground/es6_arrow_functions_2.js
@@ -1,43 +1,33 @@
-'use strict';
-
 //arguments object - no longer bound with arrow functions
 
-var add = function add(a, b) {
+const add = (a, b) => {
   // console.log(arguments)
-  return a + b;
-};
+  return a + b; 
+}
 console.log(add(10, 1, 10001));
 
 //this keyword - no longer bound with arrow functions
 
-var user = {
+const user = {
   name: 'Nacho',
   cities: ['New York', 'Baltimore', 'Philadelphia'],
-  printPlacesLived: function printPlacesLived() {
-    var _this = this;
-
-    return this.cities.map(function (city) {
-      return _this.name + ' has lived in ' + city;
-    });
+  printPlacesLived() {  
+    return this.cities.map(city => this.name + ' has lived in ' + city);
     // this.cities.forEach(city => console.log(this.name + ' has lived in ' + city));
   }
-};
+}
 
 console.log(user.printPlacesLived());
 // user.printPlacesLived();
 
 //Challange 
 
-var multiplier = {
+const multiplier = {
   numbers: [2, 4, 11, 22, 24],
   multiplyBy: 2,
-  multiply: function multiply() {
-    var _this2 = this;
-
-    return this.numbers.map(function (num) {
-      return num * _this2.multiplyBy;
-    });
+  multiply() {
+    return this.numbers.map(num => num * this.multiplyBy);
   }
-};
+}
 
 console.log(multiplier.multiply());
