1bcd45c70351455c3b9dcb2e162666604389e9b2
SnowSurfer <drinkaziu@gmail.com>
2018-12-25 19:47:16 -0500

    Conditional Rendering in JSX

--- src/app.js
+++ src/app.js
@@ -4,13 +4,19 @@ console.log("App.js is running");
 
 let app = {
   title: 'Indecision App',
-  subtitle: 'Put your life in the hands of a computer'
+  subtitle: 'Put your life in the hands of a computer',
+  options: ['One', 'Two']
 };
 
 let template = (
   <div>
     <h1>{app.title}</h1>
-    <p>{app.subtitle}</p>
+    {app.subtitle && <p>{app.subtitle}</p>}
+    <p>{app.options.length > 0 ? 'Here are your options:' : 'No Options'}</p>
+    <ol>
+      <li>Item One</li>
+      <li>Item Two</li>
+    </ol>
   </div>
 );
 
@@ -20,12 +26,17 @@ let user = {
   location: 'New York'
 }
 
+function getLocation(location) {
+  if(location) {
+    return <p>Location: {location}</p>
+  }
+}
 
 let templateTwo = (
   <div>
-    <h1>{user.name}</h1>
-    <p>Age: {user.age}</p>
-    <p>Location: {user.location}</p>
+    <h1>{user.name ? user.name : 'Anonimous'}</h1>
+    {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+    {getLocation(user.location)}
   </div>
 );
 
